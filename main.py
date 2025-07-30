eb6e3940c4081929cacec68683a4a4f5628085f5
eldelgas04 <reda-samid18@hotmail.com>
2025-07-30 02:19:59 +0200

    uppload main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 SCRIPTS = [
     "scripts/0_generate_metadata.py",     # Generar título y descripción
@@ -9,12 +10,12 @@ SCRIPTS = [
     "scripts/3_generate_audio.py",
     "scripts/download_video.py",
     "scripts/4_generate_video.py",
-    "scripts/upload_video.py"             # Subir a YouTube
+    "scripts/upload_to_youtube.py"             # Subir a YouTube
 ]
 
 def run_script(script_path):
     print(f"\n🚀 Ejecutando {script_path}...\n")
-    result = subprocess.run(["python", script_path])
+    result = subprocess.run([sys.executable, script_path])
     if result.returncode != 0:
         print(f"❌ Error al ejecutar {script_path}")
         exit(1)
